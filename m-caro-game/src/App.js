7681e3d86922c8a1a7705e4ee64d9f6d595c9e86
huyhnueit68 <huypq_sv@hnue.edu.vn>
2021-07-31 08:14:25 +0700

    add auth0

--- m-caro-game/src/App.js
+++ m-caro-game/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomeContainer from "./containers/HomeContainer"
+import Auth0ProviderWithHistory from "./components/auth/auth0-provider-with-history";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import Loading from "./components/loading";
 
 function App() {
+
+  // const { isLoading } = useAuth0();
+
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
+
   return (
     <BrowserRouter>
       <Switch>
+        <Auth0ProviderWithHistory>
           <Route exact path="/" component={HomeContainer}/>
+        </Auth0ProviderWithHistory>
       </Switch>
     </BrowserRouter>
   );
